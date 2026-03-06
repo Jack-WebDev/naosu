b44be466d4fd101843e0ccc2dddeb7318275b913
Jack-WebDev <jackbuda31@gmail.com>
2026-03-06 03:01:15 +0200

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@naosu/db";
+import * as schema from "@naosu/db/schema/auth";
+import { env } from "@naosu/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
